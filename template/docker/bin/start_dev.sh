52fc2a1d5fdbdedeb8551f9b5318d0f0c23cfe1c
vidalmatheus <matheusvidaldemenezes@gmail.com>
2021-11-21 18:52:05 -0300

    Update start_dev.sh

--- template/docker/bin/start_dev.sh
+++ template/docker/bin/start_dev.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+./manage.py collectstatic --noinput
 ./manage.py migrate --noinput
 cd /app
 DJANGO_DEBUG=1 ./manage.py runserver 0.0.0.0:8000 &
 cd /app/frontend
-NODE_ENV=development PORT=3001 HOST=0.0.0.0 API_MOCK=1 npm run dev &
-NODE_ENV=development PORT=3000 HOST=0.0.0.0 API_MOCK=0 BASE_URL=http://localhost:8000 npm run dev
+NODE_ENV=development HOST=0.0.0.0 npm run dev &
+NODE_ENV=development HOST=0.0.0.0 BASE_URL=http://localhost:8000 npm run dev:disable-mock
